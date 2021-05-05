120e658c5a65d645f7f7cea0d9d99ca0477b01a4
sarabastian <seb118@georgetown.edu>
2021-05-05 12:12:03 -0400

    app w/o redux

--- frontend/src/components/Day.js
+++ frontend/src/components/Day.js
@@ -1,16 +1,44 @@
 import React from "react";
 import "../Day.css";
 import Task from "./Task";
+import { API_URL } from "../apiConstants";
+
 
 const Day = ({ day }) => {
   const date = day.date.split("-").reverse().join("-");
+// const [allTasks, setAllTasks] = React.useState([])
+  const [newTask, setNewTask] = React.useState('')
+    const [open, setOpen] = React.useState(false)
+    const openModal = () => {
+        setOpen(true)
+    }
+//   const setAllTasks
+ const addTask = (e) => {
+     setNewTask(e.target.value);
+    fetch(API_URL + '/tasks')
+    .then(r=> r.json())
+    .then(task => )
+ }
+
+
   return (
     <div className="box">
       <h1>To Do on {date}</h1>
       {day.tasks.map((t) => (
         <Task key={t.id} task={t} />
       ))}
-      <button className="add-btn"> Add Task</button>
+      <button onClick={openModal} className="add-btn">
+        {" "}
+        <strong>Add Task</strong>
+      
+      </button>
+      {open ? <div className="modal">
+      <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+      </div>
+      : null}
     </div>
   );
 };
