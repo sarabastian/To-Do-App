2719bd4f784f975d3f03417c92f1e8c3414f0631
sarabastian <seb118@georgetown.edu>
2021-05-11 10:03:24 -0400

    navbar

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -3,6 +3,7 @@ import "../App.css";
 import Day from "./Day";
 import { useSelector, useDispatch } from "react-redux";
 import getDays from "../redux/actions/getDays";
+import "../Main.css";
 
 const Main = () => {
   const days = useSelector((state) => {
@@ -16,6 +17,10 @@ const Main = () => {
 
   return (
     <main>
+      <nav>
+        {" "}
+        <button className="day-btn">Add Day</button>
+      </nav>
       {days.map((d) => (
         <Day key={d.id} day={d} />
       ))}
